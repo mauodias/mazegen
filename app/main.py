d9f790fecb0b7835cd00e3278abcf881d958ca15
Mauricio Dias <mau.dias@gmail.com>
2019-10-02 11:25:57 -0300

    Creates main.py file with entrypoint for cloud function

---
+++ app/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+import draw
+import maze
+
+def run(rows=40, cols=40, cell_size=10, initial_x=0, initial_y=0, final_x=20, final_y=39):
+    drawer = draw.Drawer('temp.svg')
+    m = maze.Maze(rows, cols, cell_size, drawer)
+    m.generate(initial_x, initial_y, final_x, final_y)
+    m.draw()
+    out = open('temp.svg').read()
+    return out
+
+def clean():
+    os.remove('temp.svg')
+
+def entry():
+    svg = run()
+    clean()
+    return svg
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        run()
+    elif len(sys.argv) == 9:
+        rows = sys.argv[1]
+        cols = sys.argv[2]
+        cell_size = sys.argv[3]
+        initial_x = sys.argv[4]
+        initial_y = sys.argv[5]
+        final_x = sys.argv[6]
+        final_y = sys.argv[7]
+        run()
+    else:
+        print('Incorrect parameters')
